e49882f9b66d8f3c5400ea4ae136a9759c96a4f1
BlaStrijder <Blastrijder@gmail.com>
2026-01-07 12:02:35 +0100

    Update recommendation.controller.ts

--- src/api/controllers/ai/recommendation.controller.ts
+++ src/api/controllers/ai/recommendation.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Get, Post, Body, Param, Patch, Delete, NotFoundException } from '@nestjs/common';
+import { RecommendationsService } from 'src/application/ai/recommendation.service';
 
 @Controller('recommendations')
 export class RecommendationController {
   constructor(
-    private readonly generateRecommendations: GenerateRecommendationsService,
+    private readonly recommendations: RecommendationsService,
   ) {}
 
   @Post()
   async generate(@Body() body: { userId: string }) {
     // Controller is only a wrapper
-    return this.generateRecommendations.execute(body.userId);
+    return this.recommendations.execute(body.userId);
   }
 }
