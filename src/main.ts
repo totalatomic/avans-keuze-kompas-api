bf13e56a2f04ba0f544bd513ff8816ab14c9175a
mikeyboy52 <mikevanlommel2007@gmail.com>
2026-01-16 09:34:08 +0100

    fixed cors online front-end

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,8 @@ function setUpAppConfiguration(app: INestApplication, configService: ConfigServi
 ): void {
   app.enableCors({
     origin: [
-      'http://localhost:5173',
+      configService.get<string>('WEBAPP_BASE_URL_PROD'),
+      'https://localhost:5173',
       configService.get<string>('AI_BASE_URL_PROD'),
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
